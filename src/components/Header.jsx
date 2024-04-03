0165abf2e5234b5e9153d2c7e82425620d29be70
Adrián Del Moral Martín <iFullStart@gmail.com>
2024-04-03 13:25:33 +0200

    Add project to git

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  const name = "Adrián"
+  const total = 100 + 10
+
+  return (
+    <>
+      <h1>Hola: {name}</h1>
+      <p>Total a pagar: {total}</p>
+    </>
+    )
+}
