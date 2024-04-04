fcda4dee714e01f740c6a2d5f34027de70b4c01b
Adrián Del Moral Martín <iFullStart@gmail.com>
2024-04-04 03:34:15 +0200

    Creating the dinamic cart and adding to cart items and quantity

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,37 @@ import { db } from "./data/db"
 
 function App() {
 
-  // si no sabe sel estado inicial, inicializar con vacio/array vacio
-  const [data, setData] = useState(db)
-  
   /* // si no queremos utilizar dependencias, y fuese una api:
   const [data, setData] = useState([])
   useEffect(() =>{
     setData(db)
   }, []) */
 
+  // si no sabes el estado inicial, inicializar con vacio/array vacio
+  const [data, setData] = useState(db)
+  
+  const [cart, setCart] = useState([])
+  
+  function addToCart(item) {
+    //? Inmutabilidad en react / State inmutable: que no puede cambiarse, si se ha de trabajar con un array, se debe duplicar y modificar
+
+    //* Comprobar si existe el elemento uno en el array igual
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id) // Dará 0 si existe, -1 si no existe
+    //* Prevenir que se agrege de nuevo en el array
+    if(itemExists >= 0){ // Existe en el carrito
+      const updatedCart = [...cart]
+      //* Añadirá si existe, más cantidades de ese item
+      updatedCart[itemExists].quantity++
+      setCart(updatedCart)
+    } else {
+      item.quantity = 1
+      setCart(prevCart => [...prevCart, item])    
+    }
+  }
+
   return (
     <>
-
       <Header/>
-
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
         <div className="row mt-5">
@@ -26,13 +43,14 @@ function App() {
               <Guitar 
                 key={guitar.id}
                 guitar={guitar}
+                setCart={setCart}
+                addToCart={addToCart}
               />
           ))}
-          
         </div>
       </main>
-
-        <footer className="bg-dark mt-5 py-5">
+      
+      <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
           <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
         </div>
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,6 +1,8 @@
-export default function Guitar({guitar}) {
+export default function Guitar({guitar, addToCart}) {
   
   const { id, name, image, description, price } = guitar
+
+  
   
   return (
       <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -15,7 +17,9 @@ export default function Guitar({guitar}) {
           <p>{description}</p>
           <p className="fw-black text-primary fs-3">{price} €</p>
           <button type="button"
-            className="btn btn-dark w-100">Agregar al Carrito</button>
+            className="btn btn-dark w-100"
+            onClick={() => addToCart(guitar)}
+            >Agregar al Carrito</button>
         </div>
       </div>
     )
