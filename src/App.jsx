e1ad4f86f6517278e50ce8479ea85cfb48f57841
Adrián Del Moral Martín <iFullStart@gmail.com>
2024-04-06 12:50:17 +0200

    Cart add and delete items created

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,10 @@ function App() {
 
   // si no sabes el estado inicial, inicializar con vacio/array vacio
   const [data, setData] = useState(db)
-  
   const [cart, setCart] = useState([])
+  
+  const MIN_ITEMS = 1;
+  const MAX_ITEMS = 5;
   
   function addToCart(item) {
     //? Inmutabilidad en react / State inmutable: que no puede cambiarse, si se ha de trabajar con un array, se debe duplicar y modificar
@@ -23,6 +25,7 @@ function App() {
     const itemExists = cart.findIndex((guitar) => guitar.id === item.id) // Dará 0 si existe, -1 si no existe
     //* Prevenir que se agrege de nuevo en el array
     if(itemExists >= 0){ // Existe en el carrito
+      if (cart[itemExists].quantity >= MAX_ITEMS) return // si no es mayor o igual a la cantidad, incrementará las cantidades desde el botón del item
       const updatedCart = [...cart]
       //* Añadirá si existe, más cantidades de ese item
       updatedCart[itemExists].quantity++
@@ -37,12 +40,46 @@ function App() {
     console.log(`Eliminando..., ${id}`);
     setCart(prevCart => prevCart.filter(guitar => guitar.id !== id)) // devolverá las guitarrá diferentes a la que hemos seleccionado
   }
+  function decreaseQuantity(id) {
+      const updatedCart = cart.map(item => {
+        if(item.id === id && item.quantity > MIN_ITEMS) {
+            return{
+                ...item, 
+                quantity: item.quantity - MIN_ITEMS
+          }
+        }
+        return item
+      })
+      setCart(updatedCart)
+  }
+
+  function increaseQuantity(id) {
+    
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity < MAX_ITEMS) {
+        return{
+          ...item, 
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    
+    setCart(updatedCart)
+  }
+
+  function clearCart() {
+    setCart([])
+  }
 
   return (
     <>
       <Header 
         cart={cart}
         removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
       />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react"
 
-export default function Header({cart, removeFromCart}) {
+export default function Header({cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart}) {
   
   // State Derivado
   // true si está vacio / false si está con items
@@ -54,12 +54,14 @@ export default function Header({cart, removeFromCart}) {
                               </td>
                               <td className="flex align-items-start gap-4">
                                 <button type="button"
-                                  className="btn btn-dark">
+                                  className="btn btn-dark"
+                                onClick={() => decreaseQuantity(guitar.id)}>
                                   -
                                 </button>
                                 {guitar.quantity}
                                 <button type="button"
-                                  className="btn btn-dark">
+                                  className="btn btn-dark"
+                                  onClick={() => increaseQuantity(guitar.id)}>
                                   +
                                 </button>
                               </td>
@@ -78,7 +80,12 @@ export default function Header({cart, removeFromCart}) {
                       <p className="text-end">Total pagar: <span className="fw-bold">{cartTotal} €</span></p>
                     </>
                   )}
-                  <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                  
+                  <button className="btn btn-dark w-100 mt-3 p-2" 
+                    onClick={clearCart}
+                  >
+                    Vaciar Carrito
+                  </button>
                 </div>
               </div>
             </nav>
